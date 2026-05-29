3a509a142f4e95824992361c4891fc1930a6fb08
Ubuntu <ubuntu@ip-172-31-6-155.ec2.internal>
2026-05-29 16:22:31 +0000

    Added Docker build automation to jenkins pipeline v4

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ DB_CONFIG = {
 
 @app.route("/")
 def home():
-    return "Flask + PostgreSQL running. Jenkis is working fine too.Yay!"
+    return "Flask + PostgreSQL running. Jenkins pipeline feature is added.Yay!"
 
 @app.route("/db-check")
 def db_check():
